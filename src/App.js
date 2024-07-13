5b4bf5668de84f43a4a012653de5d3f4c954e460
T-Godwins <gtuyishime@gmail.com>
2024-07-13 00:29:10 -0400

    Button and Tryna get more suffisticated

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import About from './About';
 import './About.css'
 import Projects from './Projects';
+import './Projects.css'
 import Skills from './Skills';
+import './Skills.css'
 import Contact from './Contact';
+import './Contact.css'
 import './App.css';
 
 function App() {
@@ -13,7 +16,9 @@ function App() {
         <h1>Hello, I am</h1>
         <h2>Godwins Tuyishime</h2>
         <nav>
+          <button>
           <a href="https://drive.google.com/file/d/168XfY4fgx6ERkzLJKtzPMrg_eiRtVfQ6/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
+          </button>
           <a href="https://linkedin.com/in/godwins-tuyishime" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           <a href="https://github.com/T-Godwins" target="_blank" rel="noopener noreferrer">GitHub</a>
         </nav>
